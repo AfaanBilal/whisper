573a43a5d9994860d258f11d98e81cc16af56cdf
Afaan Bilal <afaanbilal@gmail.com>
2023-07-09 14:50:35 +0530

    ✨ Home

--- utils/posts.go
+++ utils/posts.go
@@ -33,3 +33,43 @@ func PostCount(userId uint) int64 {
 	database.DB.Where("user_id = ?", userId).Model(&post).Count(&count)
 	return count
 }
+
+func FindUser(users []models.User, userId uint) models.User {
+	for _, user := range users {
+		if user.ID == userId {
+			return user
+		}
+	}
+
+	return users[0]
+}
+
+func LikeCount(postId uint) int64 {
+	var like models.Like
+	var count int64
+	database.DB.Where("post_id = ?", postId).Model(&like).Count(&count)
+	return count
+}
+
+func LikeCounts(postIds []uint) map[uint]int64 {
+	var like models.Like
+	var counts map[uint]int64
+	database.DB.Model(&like).Where("post_id IN ?", postIds).Select("post_id, COUNT(post_id) as count").Group("post_id").Find(&counts)
+	return counts
+}
+
+func LikedPosts(userId uint, postIds []uint) []models.Like {
+	var likes []models.Like
+	database.DB.Where("user_id = ? AND post_id IN ?", userId, postIds).Find(&likes)
+	return likes
+}
+
+func HasLiked(userId uint, postId uint, likes []models.Like) bool {
+	for _, like := range likes {
+		if like.PostId == postId && like.UserId == userId {
+			return true
+		}
+	}
+
+	return false
+}
